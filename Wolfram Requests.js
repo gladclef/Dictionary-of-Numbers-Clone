f4d2de50e98be2d501026da69edc5a2ab618aed6
Benjamin Bean <gladclef@gmail.com>
2016-11-27 21:57:57 -0700

    added basic description, added example results from wolfram alpha, added js files for making requests to wolfram

---
+++ Wolfram Requests.js
@@ -0,0 +1,6 @@
+var queryPart1 = "http://api.wolframalpha.com/v2/query?appid=";
+var queryPart2 = "&includepodid=Comparison&scanner=Unit&format=plaintext&";
+function makeWolframRequest(input)
+{
+	var query = queryPart1 + getAppId() + queryPart2 + input;
+}
